3edcb7234b9031602d49abdb52a1749ff48b9f49
Tahira Jannat <jannat.setu95@gmail.com>
2024-02-14 18:38:33 +0600

    Header, banner, brand components designed

--- src/components/common/PopularBlock.jsx
+++ src/components/common/PopularBlock.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
+import Popular2 from '../../assets/product/item-category-1.png';
+import Popular3 from '../../assets/product/item-category-2.png';
+import Popular4 from '../../assets/product/item-category-3.png';
+import Popular1 from '../../assets/product/item-category.png';
 
 export default function PopularBlock() {
+    const popularProducts = [
+        {
+            id: 1,
+            name: Popular1,
+            link: '/',
+            category: '',
+            totalItems: 10,
+        },
+        {
+            id: 2,
+            name: Popular2,
+            link: '/',
+            category: '',
+            totalItems: 10,
+        },
+        {
+            id: 3,
+            name: Popular3,
+            link: '/',
+            category: '',
+            totalItems: 10,
+        },
+        {
+            id: 4,
+            name: Popular4,
+            link: '/',
+            category: '',
+            totalItems: 10,
+        },
+    ];
     return (
         <div className='container mx-auto px-20'>
             <div className='grid grid-cols-12 gap-4'>
                 <div className='col-span-6'>
-                    <img src='' alt='' srcset='' />
+                    {popularProducts.slice(0, 1).map((product) => {
+                        <div key={product.id}>
+                            <img src={product.name} alt='' srcset='' />
+                        </div>;
+                    })}
                 </div>
                 <div className='col-span-6 grid grid-col-2 gap-4'>
-                    <img src='' alt='' srcset='' />
+                    {popularProducts.slice(1, 10).map((product) => {
+                        <div key={product.id}>
+                            <img src={product.name} alt='' srcset='' />
+                        </div>;
+                    })}
                 </div>
             </div>
         </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BannerPattern from '../../assets/top-main.png';
 import Brands from '../common/Brands';
 import HeroBanner from '../common/HeroBanner';
+import PopularBlock from '../common/PopularBlock';
 import HeaderBottom from '../header/HeaderBottom';
 import HeaderTop from '../header/HeaderTop';
 
@@ -17,6 +18,7 @@ export default function Home() {
                 <HeroBanner />
             </div>
             <Brands />
+            <PopularBlock />
         </>
     );
 }
