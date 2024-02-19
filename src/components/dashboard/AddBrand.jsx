17d6e75e8be46bbccd6a5e902f1541d6b0ffa3c4
Tahira Jannat <jannat.setu95@gmail.com>
2024-02-19 18:58:38 +0600

    brand value inputs  added to database

--- src/components/dashboard/AddBrand.jsx
+++ src/components/dashboard/AddBrand.jsx
@@ -1,122 +1,58 @@
 import { addDoc, collection } from 'firebase/firestore';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import React, { useState } from 'react';
 import { FaLocationArrow } from 'react-icons/fa6';
-import { v4 as uuid } from 'uuid';
-import { firestore, storage } from '../../firebase/index';
+import { firestore } from '../../firebase/index';
 import Button from '../common/Button';
-const people = [
-    { id: 1, name: 'Wade Cooper' },
-    { id: 2, name: 'Arlene Mccoy' },
-    { id: 3, name: 'Devon Webb' },
-    { id: 4, name: 'Tom Cook' },
-    { id: 5, name: 'Tanya Fox' },
-    { id: 6, name: 'Hellen Schmidt' },
-    { id: 7, name: 'Caroline Schultz' },
-    { id: 8, name: 'Mason Heaney' },
-    { id: 9, name: 'Claudie Smitham' },
-    { id: 10, name: 'Emil Schaefer' },
-];
+
 export default function AddBrand() {
-    const productsRef = collection(firestore, 'products');
+    const productsRef = collection(firestore, 'brands');
 
     const [formData, setFormData] = useState({
-        title: '',
-        category: '',
-        price: '',
-        salePrice: '',
-        description: '',
-        src: '',
+        brand_name: '',
+        brand_description: '',
+        brand_slug: '',
     });
 
-    const [imageUpload, setImageUpload] = useState(null);
-    const [uploadedImageUrl, setUploadedImageUrl] = useState('');
     const [loading, setLoading] = useState(false);
-
     const handleChange = (e) => {
         const { name, value } = e.target;
+        let slugValue;
+
+        if (name === 'brand_name') {
+            slugValue = value
+                .toLowerCase()
+                .replace(/\s+/g, '-')
+                .replace(/[^a-z0-9-]/g, '');
+        }
+
         setFormData((prevData) => ({
             ...prevData,
             [name]: value,
+            ...(slugValue && { brand_slug: slugValue }),
         }));
-        e.target.reset();
-    };
-
-    const handleFileSelect = (event) => {
-        setImageUpload(event.target.files[0]);
     };
 
     const handleSaveAndUpload = async (e) => {
         e.preventDefault();
 
-        if (!imageUpload) {
-            console.log('Please select an image');
-            return;
-        }
-
-        const storageRef = ref(storage, 'products/' + uuid());
-
         try {
-            // Upload image
-            await uploadBytes(storageRef, imageUpload);
-            const imageUrl = await getDownloadURL(storageRef);
-            setUploadedImageUrl(imageUrl);
-
-            // Save data
             const newData = {
-                title: formData.title,
-                description: formData.description,
-                category: formData.category,
-                brand: formData.brand,
-                price: formData.price,
-                salePrice: formData.salePrice,
-                discountPercentage: formData.discountPercentage,
-                stock: formData.stock,
-                image: formData.imageUrl,
-                deal: formData.deal,
-                stockStatus: formData.stockStatus,
-                createdAt: formData.createdAt,
-                updatedAt: formData.updatedAt,
+                brand_name: formData.brand_name,
+                brand_description: formData.brand_description,
+                brand_slug: formData.brand_slug,
             };
             setLoading(true);
-            // Save data to the 'products' collection
             await addDoc(productsRef, newData);
 
-            console.log('Product added successfully!');
-            // Reset form data
-            // setFormData({
-            //     title: '',
-            //     category: '',
-            //     price: '',
-            //     salePrice: '',
-            //     description: '',
-            //     src: '',
-            // });
+            console.log('Brand added successfully!');
+
             setFormData({
-                // id: '',
-                title: '',
-                description: '',
-                category: '',
-                brand: '',
-                price: '',
-                discountPercentage: '',
-                stock: '',
-                image: '',
-
-                deal: '',
-                status: {
-                    bestSeller: '',
-                    newArrival: '',
-                },
-                createdAt: '',
-                updatedAt: '',
+                brand_name: '',
+                brand_description: '',
+                brand_slug: '',
             });
-
-            // Clear the input field for file
-            setImageUpload(null);
         } catch (error) {
-            console.error('Error uploading image or adding product:', error);
-            // Call your error handling function here (e.g., toastifyError)
+            console.error('Error adding brand:', error);
         } finally {
             setLoading(false);
         }
@@ -134,15 +70,15 @@ export default function AddBrand() {
                     <div className='my-4'>
                         <label
                             className='text-black my-6 capitalize'
-                            htmlFor='productTitle'
+                            htmlFor='brand_name'
                         >
                             Brand Name
                         </label>
                         <input
-                            id='productTitle'
+                            id='brand_name'
                             type='text'
-                            name='title'
-                            value={formData.title}
+                            name='brand_name'
+                            value={formData.brand_name}
                             onChange={handleChange}
                             className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white  rounded-md dark:bg-gray-200 dark:text-gray-500 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
                         />
@@ -151,15 +87,15 @@ export default function AddBrand() {
                     <div className='my-4'>
                         <label
                             className='text-black my-6 capitalize'
-                            htmlFor='productTitle'
+                            htmlFor='brand_slug'
                         >
                             Brand slug
                         </label>
                         <input
-                            id='productTitle'
+                            id='brand_slug'
                             type='text'
-                            name='title'
-                            value={formData.title}
+                            name='brand_slug'
+                            value={formData.brand_slug}
                             onChange={handleChange}
                             className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white  rounded-md dark:bg-gray-200 dark:text-gray-500 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
                         />
@@ -169,14 +105,14 @@ export default function AddBrand() {
                     <div className='my-4'>
                         <label
                             className='text-black my-6 capitalize'
-                            htmlFor='productDescription'
+                            htmlFor='brand_description'
                         >
                             brand Description
                         </label>
                         <textarea
-                            id='productDescription'
-                            name='description'
-                            value={formData.description}
+                            id='brand_description'
+                            name='brand_description'
+                            value={formData.brand_description}
                             onChange={handleChange}
                             className='block w-full px-4 py-2 h-52 mt-2 text-gray-700 bg-white  rounded-md dark:bg-gray-200 dark:text-gray-500 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring'
                         ></textarea>
